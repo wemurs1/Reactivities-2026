b621f949f57a28e95008b230dda7c5528c553dc7
Stuart Murray <stuart.murray.dev@outlook.com>
2026-02-06 16:37:42 +0000

    section 2

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,9 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Activity> Activities { get; set; }
+}
