774751ff80a1bf3948cf50e257d7a7b14e2d2c22
Stuart Murray <stuart.murray.dev@outlook.com>
2026-02-13 16:14:22 +0000

    End of section 9

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,13 +1,17 @@
 import { Group } from "@mui/icons-material";
-import { Box, AppBar, Toolbar, Container, MenuItem, Typography } from "@mui/material";
+import { Box, AppBar, Toolbar, Container, MenuItem, Typography, LinearProgress } from "@mui/material";
 import { NavLink } from 'react-router';
 import MenuItemLink from '../shared/components/MenuItemLink';
+import { useStore } from '../../lib/hooks/useStore';
+import { Observer } from 'mobx-react-lite';
 
 
 export default function NavBar() {
+  const { uiStore } = useStore();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)' }}>
+      <AppBar position="static" sx={{ backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)', position:'relative' }}>
         <Container maxWidth='xl'>
           <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
             <Box>
@@ -23,10 +27,18 @@ export default function NavBar() {
               <MenuItemLink to='/createActivity'>
                 Create Activity
               </MenuItemLink>
+              <MenuItemLink to='/counter'>
+                Counter
+              </MenuItemLink>
             </Box>
             <MenuItem>User Menu</MenuItem>
           </Toolbar>
         </Container>
+        <Observer>
+          {() => uiStore.isLoading ? (
+            <LinearProgress color='secondary' sx={{ position: 'absolute', bottom: 0, left: 0, right: 0, height: 4 }} />
+          ) : null}
+        </Observer>
       </AppBar>
     </Box>
   )
---
+++ client/src/lib/stores/store.ts
@@ -0,0 +1,16 @@
+import { createContext } from 'react';
+import CounterStore from './counterStore';
+import { UiStore } from './UiStore';
+
+interface Store {
+    counterStore: CounterStore
+    uiStore: UiStore
+}
+
+export const store: Store = {
+    counterStore: new CounterStore(),
+    uiStore: new UiStore()
+}
+
+export const StoreContext = createContext(store)
+
